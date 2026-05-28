4305e3c6f7bc70e1679c1413413e44248556ee7e
Jace Morgan <137754421+jmorg23@users.noreply.github.com>
2026-05-28 09:47:06 -0600

    arrays

---
+++ ponder/week6/arrays.js
@@ -0,0 +1,53 @@
+const numbers = [65, 44, 12, 4];
+numbers.forEach(myFunction)
+
+function myFunction(item) {
+  console.log(item * 10); 
+}     
+                        
+                                  
+const numbers = [65, 44, 12, 4];
+const newArr = numbers.map(myFunction)
+
+function myFunction(num) {
+  return num * 10;
+}         
+                        
+                      
+const ages = [32, 33, 16, 40];
+const result = ages.filter(checkAdult);
+
+function checkAdult(age) {
+  return age >= 18;
+} 
+                        
+                    
+                      
+const numbers = [125, 20, 5];
+
+document.getElementById("output").innerHTML = numbers.reduce(myFunc);
+
+function myFunc(total, num) {
+  return total - num;
+}
+                        
+
+                            
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let index = fruits.indexOf("Apple");
+                        
+                    
+
+                        
+const movieSummary = `
+  <div class="movie-summary">
+    <h2>${movie.title}</h2>
+    <p>${movie.genre} - ${stars}</p>
+  </div>
+`;
+
+document.getElementById("movie-list").innerHTML += movieSummary;            
+
+                        
+const car = {type:"Fiat", model:"500", color:"white"};       
+                        
