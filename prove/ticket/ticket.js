f4bc3ed0fc44003707f9b0a47cb4c0a61b1a244c
Jace Morgan <137754421+jmorg23@users.noreply.github.com>
2026-06-06 15:35:55 -0600

    ticket done

--- prove/ticket/ticket.js
+++ prove/ticket/ticket.js
@@ -4,25 +4,35 @@
 const form = document.querySelector("#fsyForm");
 const travelRange = document.querySelector("#travelRange");
 const notesContainer = document.querySelector("#notesContainer");
+const notesContainer2 = document.querySelector("#notesContainer2");
+
 const notes = document.querySelector("#notes");
 const output = document.querySelector("#output");
 const campusBoxes = document.querySelectorAll('input[name="campus"]');
 
 function updateNotesField() {
-  const value = travelRange.value;
+    const value = travelRange.value;
 
-  if (value == "guest") {
-    notesContainer.style.display = "block";
-    document.querySelector("#acode").required = true;  
-    // acode.required = true;
-    console.log("val=", value);
-  } else {
-        document.querySelector("#acode").required = false;  
+    if (value == "guest") {
+        notesContainer.style.display = "block";
+        document.querySelector("#acode").required = true;
 
-    notesContainer.style.display = "none";
-    console.log("valllue=", value);
+        notesContainer2.style.display = "none";
+        document.querySelector("#scode").required = false;
+        // acode.required = true;
+        console.log("val=", value);
+    } else if (value == "student") {
+        document.querySelector("#acode").required = false;
+        notesContainer.style.display = "none";
+        notesContainer2.style.display = "block";
 
-  }
+        notesContainer.style.display = "none";
+        console.log("valllue=", value);
+
+    }else{
+        notesContainer.style.display = "none";
+        notesContainer2.style.display = "none";
+    }
 }
 
 travelRange.addEventListener("change", updateNotesField);
@@ -30,31 +40,31 @@ updateNotesField();
 
 
 function isDateValid(value) {
-  const today = new Date();
-  const chosen = new Date(value);
-  return chosen > today;
+    const today = new Date();
+    const chosen = new Date(value);
+    return chosen > today;
 }
 
 
 form.addEventListener("submit", event => {
-  event.preventDefault();
-  output.textContent = "";
+    event.preventDefault();
+    output.textContent = "";
 
-  const firstName = form.firstName.value.trim();
-  const lastName = form.lastName.value.trim();
-  const email = form.email.value.trim();
+    const firstName = form.firstName.value.trim();
+    const lastName = form.lastName.value.trim();
+    const email = form.email.value.trim();
 
-  const numberOfCampuses = form.travelRange.value;
+    const numberOfCampuses = form.travelRange.value;
 
-  const availableDate = form.availableDate.value;
+    const availableDate = form.availableDate.value;
 
 
-  if (!isDateValid(availableDate)) {
-    output.textContent = "Please choose a later date.";
-    return;
-  }
+    if (!isDateValid(availableDate)) {
+        output.textContent = "Please choose a later date.";
+        return;
+    }
 
-  output.innerHTML = `
+    output.innerHTML = `
   <h2>Preference Submitted</h2>
   <p>${firstName} ${lastName}</p>
   <p>Email: ${email}</p>
@@ -64,7 +74,6 @@ form.addEventListener("submit", event => {
   
   `;
 
-  form.reset();
-  updateNotesField();
+    form.reset();
+    updateNotesField();
 });
-          
