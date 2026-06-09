df98f76876f19b011eeaba73e85676732490f77d
Jace Morgan <137754421+jmorg23@users.noreply.github.com>
2026-06-09 09:56:05 -0600

    objects

---
+++ ponder/week8/objects.js
@@ -0,0 +1,51 @@
+
+const aCourse = {
+    code: 'WDD131',
+    name: 'Dynamic web',
+
+    sections: [
+        { sectionNum: 1, 
+            roomNum: 'STC 353', 
+            enrolled: 2, days: 'TTh', 
+            instructor: 'Bro T' },
+
+        { sectionNum: 2, 
+            roomNum: 'STC 347', 
+            enrolled: 8, 
+            days: 'TTh', 
+            instructor: 'Sis A' }
+    ],
+
+
+    enrollStudent: function (sectionNum) {
+
+        const sectionIndex = this.sections.findIndex(
+            (section) => section.sectionNum == sectionNum
+        );
+        if (sectionIndex >= 0) {
+            this.sections[sectionIndex].enrolled++;
+            renderSections(this.sections);
+        }
+    }
+};
+
+function sectionTemplate(section) {
+    return `<tr>
+      <td>${section.sectionNum}</td>
+      <td>${section.roomNum}</td>
+      <td>${section.enrolled}</td>
+      <td>${section.days}</td>
+      <td>${section.instructor}</td></tr>`
+}
+
+function renderSections(sections) {
+    const html = sections.map(sectionTemplate);
+    document.querySelector("#sections").innerHTML = html.join("");
+}
+
+renderSections(aCourse.sections);
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.enrollStudent(sectionNum);
+});
